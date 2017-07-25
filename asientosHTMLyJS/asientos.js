36b31a739012f23b9b9241650fdd099b0d7489cf
sonia526 <zamirastars3@gmail.com>
2017-07-25 10:44:04 -0500

    modificado

--- asientosHTMLyJS/asientos.js
+++ asientosHTMLyJS/asientos.js
@@ -1,56 +1,137 @@
-var asientos = document.getElementsByTagName('td');
-for (var i = 0; i < asientos.length; i++) {
-    asientos[i].addEventListener('click',redirect,false);
+var celdas = document.getElementsByTagName('td');
+for (var i = 0; i < celdas.length; i++) {
+    celdas[i].addEventListener('click',redirect,false);
 }
+
+var numAsiento="";
+var celdaActual;
+
 function redirect(event){
-    var asiento=(event.target.textContent);
-    var mostrar=document.getElementById("mostrar");
-    mostrar.innerHTML=asiento;
-    (event.target).style.backgroundColor = ((event.target).style.backgroundColor=='rgb(248, 237, 80)') ? 'transparent' : '#F8ED50';
-}
-function buscar(ingreso){
-	var ingreso=document.getElementById("nro_dni").value;
-  var operacion= ingreso*2;
-  document.getElementById("nro_dni").value=operacion;
-}
-function reserva(){
-//hola lo de abajo es mi inplementacion
-  var nombre =document.getElementById("nombre").value;
-  var apellido= document.getElementById("apellido").value;
-  var dni=document.getElementById("dni").value;
-  var resultado=getElementById("mostrar");
+    document.getElementById("mostrar").innerHTML= (event.target.textContent);
 
-      
-  reservar.innerText=resultado( document.getElementById('mostrar').value);
-}
+    var num = parseInt(event.target.textContent);
+    celdaActual = event.target;
 
+    numAsiento = event.target.textContent;
 
-function selecionar(){
+     for (var i =0; i<pasajeros.length;i++){
+        var obtiene = pasajeros[i];
 
+        var s = parseInt(obtiene.asiento);
+        if(s === num){
+            document.getElementById("nombre").value = obtiene.nombre;
+            document.getElementById("apell").value = obtiene.apellido;
+            document.getElementById("eldni").value = obtiene.iddni;
 
+        }
+    }
 }
 
+var pasajeros = [];
+console.log(pasajeros);
+var listando = document.getElementById('lista');
+
+function datos_Pasajero(asiento, nombre, apellido, iddni){
+    this.asiento = asiento;
+    this.nombre= nombre;
+    this.apellido = apellido;
+    this.iddni = iddni;
+};
+
+datos_Pasajero.prototype.toHTML = function(){
+    var html = '';
+    html += 'Asiento N° ' + this.asiento + '<br>';
+    html += 'Nombre: ' + this.nombre + '<br>';
+    html += 'Apellido: ' + this.apellido + '<br>';
+    html += 'N° de DNI: ' + this.iddni + '<br>';
+    html += '<br><br>';
+    return html;
+}
+
+function mergeHTML(){
+    var html = '';
+    for (var i=0; i<pasajeros.length; i++){
+        html += pasajeros[i].toHTML();
+    }
+    return html;
+}
+
+function printHTML (html){
+  listando.innerHTML = '';
+  listando.innerHTML = html;
+}
+
+//Funcion Agregar
+var addPas = document.getElementById('agregar');
+addPas.onclick = function() {
+    var asientos = document.getElementById("mostrar").textContent;
+    var suNombre = document.getElementById("nombre");
+    var suApellido = document.getElementById("apell");
+    var numDNI = document.getElementById("eldni");
+    suNombre = suNombre.value;
+    suApellido = suApellido.value;
+    numDNI = parseInt(numDNI.value);
 
 
+  var datos = new datos_Pasajero(asientos, suNombre, suApellido, numDNI);
+  pasajeros.push(datos);
+  //printHTML(datos.toHTML());
 
 
+ celdaActual.style.backgroundColor = '#F8ED50';
 
+    refrescar();
+};
 
+var printAll = document.getElementById('print');
+    printAll.onclick = function() {
+    printHTML(mergeHTML());
+}
 
+function refrescar(){
+    document.getElementById("nombre").value = "";
+    document.getElementById("apell").value = "";
+    document.getElementById("eldni").value = "";
+}
 
+function buscar(){
+    var buscarDni = document.getElementById("dni");
+    buscarDni = parseInt(buscarDni.value);
+    for (var i =0; i<pasajeros.length;i++){
+        var obtiene = pasajeros[i];
+        var s = obtiene.iddni;
+        if(s==buscarDni){
+            document.getElementById("nombre").value = obtiene.nombre;
+            document.getElementById("apell").value = obtiene.apellido;
+            document.getElementById("eldni").value = obtiene.iddni;
 
+        }
+    }
 
+    refrescabuscar();
 
+}
+console.log(buscar());
 
+function refrescabuscar(){
+    document.getElementById("dni").value = "";
+}
 
+function consulta(){
+    refrescar();
+}
 
+function cancela(){
+    var  num = parseInt(document.getElementById("mostrar").textContent);
 
+    for (var i =0; i<pasajeros.length;i++){
+        var obtiene = pasajeros[i];
+        var s = parseInt(obtiene.asiento);
+        if(s === num){
+            pasajeros[i] = datos_Pasajero('-1', '', '', -1);
+        }
+    }
 
+    refrescar();
 
-
-
-
-
-
-
-
+}
